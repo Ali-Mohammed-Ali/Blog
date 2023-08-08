25372b26aede10e921ebfa821288bc2652b52003
Ali-Mohammed-Ali <alimohammed8798@gmail.com>
2023-08-08 16:35:10 +0300

    commit

---
+++ client/src/components/account/Login.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material"
+
+const Login = () => {
+
+    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+    return (
+        <Box>
+            <img src={imageURL} alt="Login" />
+        </Box>
+    )
+}
+
+export default Login;
